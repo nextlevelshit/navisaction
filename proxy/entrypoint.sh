8db1a98973720e2bf4fb095b2792e4f8c799fa0e
Michael Czechowski <m.czechowski@pons.de>
2023-05-05 19:15:52 +0200

    split cert generation and app deployment

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -5,9 +5,7 @@ API_PORT=${API_PORT:-3000}
 API_CONTAINER=${API_CONTAINER:-api}
 WEB_PORT=${WEB_PORT:-8080}
 WEB_CONTAINER=${WEB_CONTAINER:-web}
-SSL_PORT=${SSL_PORT:-8081}
-SSL_CONTAINER=${SSL_CONTAINER:-ssl}
-envsubst '${APP_HOST},${API_PORT},${API_CONTAINER},${WEB_PORT},${WEB_CONTAINER},${SSL_PORT},${SSL_CONTAINER}' < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
+envsubst '${APP_HOST},${API_PORT},${API_CONTAINER},${WEB_PORT},${WEB_CONTAINER}' < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
 cat /usr/local/etc/haproxy/haproxy.cfg
 
 # Run haproxy as the haproxy user
