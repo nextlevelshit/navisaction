8b688798d58509905a2718625396a533449468c8
Michael Czechowski <m.czechowski@pons.de>
2023-05-12 00:07:31 +0200

    fix vue meta

--- web-vue/src/main.js
+++ web-vue/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import VueMeta from "vue-meta";
 import MasonryWall from "@yeger/vue-masonry-wall";
 
 import App from "./App.vue";
@@ -8,6 +7,4 @@ import "./App.css";
 const app = createApp(App);
 
 app.use(MasonryWall);
-
-app.use(VueMeta);
 app.mount("#app");
