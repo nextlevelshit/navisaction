fc8c47f8d29f99a5bd6eb2612eb1b6e1f635eacf
Michael Czechowski <m.czechowski@pons.de>
2023-05-12 01:43:29 +0200

    add pwa

--- web-vue/vue.config.js
+++ web-vue/vue.config.js
@@ -2,12 +2,15 @@ const {defineConfig} = require('@vue/cli-service');
 
 module.exports = defineConfig({
 	transpileDependencies: true,
+	pwa: {
+		name: "PEERHOF Fotoalbum ⛰️"
+	},
 	devServer: {
 		proxy: {
-			'^/(api|uploads)': {
-				target: process.env.API_RELATIVE_PATH || 'http://localhost:3000',
+			"^/(api|uploads)": {
+				target: process.env.API_RELATIVE_PATH || "http://localhost:3000",
 				changeOrigin: true
-			},
+			}
 		}
 	}
 });
